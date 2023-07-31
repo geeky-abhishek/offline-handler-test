d69e955c9b2c8a564f87c765aebfab50c12aa2fa
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2023-07-31 14:34:31 +0530

    feat: retry mechanism added

--- src/api-helper.ts
+++ src/api-helper.ts
@@ -1,30 +1,32 @@
 import axios from 'axios';
 import localForage from 'localforage';
+import { toast } from 'react-toastify';
 
 const RETRY_LIMIT = 3;
-const RETRY_DELAY_MS = 1000;
+const RETRY_DELAY_MS = 5000;
 const API_REQUESTS_STORAGE_KEY = 'apiRequests';
 
 const api = axios.create();
 
-// Function to save a failed request to offline storage
-// const saveRequestToOfflineStorage = async (config:any) => {
-//   const requestKey = // Generate a unique key for the request, e.g., using request URL and method
-//   await localForage.setItem(requestKey, config);
-// };
+// Function to generate UUID for requests
+const generateUuid = function() {
+  return String(Date.now().toString(32) + Math.random().toString(16)).replace(
+    /\./g,
+    ''
+  );
+};
 
 // Function to save a failed request to offline storage
-export const saveRequestToOfflineStorage = async (config:any) => {
+export const saveRequestToOfflineStorage = async (config: any) => {
   try {
-    const storedRequests:Array<any> = (await localForage.getItem(API_REQUESTS_STORAGE_KEY)) || [];
+    const storedRequests: Array<any> =
+      (await localForage.getItem(API_REQUESTS_STORAGE_KEY)) || [];
     storedRequests.push(config);
     await localForage.setItem(API_REQUESTS_STORAGE_KEY, storedRequests);
   } catch (error) {
     console.error('Error saving API request for offline:', error);
   }
 };
-
-
 
 export const getStoredRequests = async () => {
   try {
@@ -42,14 +44,16 @@ export const clearStoredRequests = async () => {
     console.error('Error clearing stored API requests:', error);
   }
 };
+
 // Function to perform the actual API request and handle retries
-const performRequest = async (config:any):Promise<any> => {
+const performRequest = async (config: any): Promise<any> => {
   try {
     const response = await api.request(config);
+
     return response.data;
   } catch (error) {
     if (config.retryCount < RETRY_LIMIT) {
-      await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY_MS));
       config.retryCount++;
       return performRequest(config);
     } else {
@@ -61,14 +65,10 @@ const performRequest = async (config:any):Promise<any> => {
 };
 
 // Function to send the requests to the server and handle offline sync
-export const sendRequest = async (config:any) => {
+export const sendRequest = async (config: any) => {
   try {
-    // Add custom headers here if needed
-    // config.headers = { 'Authorization': 'Bearer YOUR_ACCESS_TOKEN' };
-
-    // Set the initial retry count to 0
-    config.retryCount = 0;
-
+    config.retryCount = config.retryCount ?? 0;
+    config.id = generateUuid();
     // Perform the API request and handle retries
     return await performRequest(config);
   } catch (error) {
@@ -76,40 +76,24 @@ export const sendRequest = async (config:any) => {
   }
 };
 
-// Function to handle offline sync (call this when the user is online again)
-// export const syncOfflineRequests = async () => {
-//   const keys = await localForage.keys();
-//   for (const key of keys) {
-//     const config = await localForage.getItem(key);
-//     if (config) {
-//       await localForage.removeItem(key);
-//       try {
-//         await performRequest(config);
-//       } catch (error) {
-//         // Handle failed sync if needed
-//       }
-//     }
-//   }
-// };
 
 export const syncOfflineRequests = async () => {
-  const storedRequests:any = await getStoredRequests();
+  const storedRequests: any = await getStoredRequests();
   if (!storedRequests || storedRequests.length === 0) {
     return;
   }
 
+  toast.info(`Syncing data with server`);
   for (const request of storedRequests) {
-    
-    if (request) {
-      // await localForage.removeItem(key);
-      storedRequests.splice(storedRequests.indexOf(request), 1);
-      await localForage.setItem(API_REQUESTS_STORAGE_KEY, storedRequests);
+    if (request) {   
       try {
         await performRequest(request);
+           storedRequests.splice(storedRequests.indexOf(request), storedRequests.find((sr:any)=>sr.id===request.id)?.length );
+        await localForage.setItem(API_REQUESTS_STORAGE_KEY, storedRequests);
       } catch (error) {
+        console.log('venom: perform request', { error });
         // Handle failed sync if needed
       }
     }
   }
 };
-
